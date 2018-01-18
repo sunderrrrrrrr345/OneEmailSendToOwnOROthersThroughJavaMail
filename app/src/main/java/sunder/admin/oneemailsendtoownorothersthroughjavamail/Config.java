d1d72a37dbe0294c05ac98950c9082d4c1334fce
Sunder <sunderrrrrrrr345@users.noreply.github.com>
2018-01-18 18:21:12 +0530

    Initial commit

---
+++ app/src/main/java/sunder/admin/oneemailsendtoownorothersthroughjavamail/Config.java
@@ -0,0 +1,10 @@
+package sunder.admin.oneemailsendtoownorothersthroughjavamail;
+
+/**
+ * Created by Admin on 1/18/2018.
+ */
+
+public class Config {
+    public static final String EMAIL ="sundarsharma2018@gmail.com";
+    public static final String PASSWORD ="sundarsharma201812345";
+}
